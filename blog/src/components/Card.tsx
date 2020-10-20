c4534d4a25445a4c52fc00fa80db3b9f877442fa
anagha30 <56297688+anagha30@users.noreply.github.com>
2020-10-20 04:50:20 -0500

    Add files via upload

---
+++ blog/src/components/Card.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Card(props: { imgsrc: string | undefined; 
+    description: string | undefined; title: string; process:React.ReactNode }) {
+    return (
+        <div className="card">
+        <h4>{props.title}</h4>
+        <img src={props.imgsrc} style={{width:230,height:290,marginLeft:500}}/>
+          <div className="card_info">
+                <span className="card_category">{props.description}</span>
+          </div>
+    <div>{props.process}</div>
+        </div>
+    )
+}
+
+export default Card
